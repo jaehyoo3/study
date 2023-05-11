3f2d161a52a40492b1bdab8033b6b64b4c921107
usdev <usdev@naver.com>
2023-05-11 16:48:05 +0900

    2023.05.11

---
+++ src/main/java/com/group/libraryapp/Service/UserService.java
@@ -0,0 +1,38 @@
+package com.group.libraryapp.Service;
+
+import com.group.libraryapp.Repository.UserRepository;
+import com.group.libraryapp.Service.dto.UserCreateDTO;
+import com.group.libraryapp.Service.dto.UserDTO;
+import com.group.libraryapp.Service.dto.UserUpdateDTO;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public void updateUser(UserUpdateDTO updateDTO) {
+
+        if(userRepository.isUserNotExist(updateDTO.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.updateUserName(updateDTO.getName(), updateDTO.getId());
+    }
+    public void deleteUser(String name) {
+        if(userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+        userRepository.deleteUser(name);
+    }
+
+    public void saveUser(UserCreateDTO requset) {
+        userRepository.saveUser(requset.getName(), requset.getAge());
+    }
+
+    public List<UserDTO> getUser() {
+       return userRepository.getUser();
+    }
+}
